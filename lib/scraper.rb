02c089dc8b3b80ed17b1c9af942ba520ccbcd187
John Stassen <john.stassen@gmail.com>
2020-08-29 21:11:30 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,7 +26,10 @@ class Scraper
     #   course.schedule = post.css(".date").text
     #   course.description = post.css("p").text
     # end
+  end
 
+  def get_courses
+    self.get_page.css(".post")
   end
 
 end
